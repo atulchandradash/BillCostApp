9ca3fe22e1690299bf97c489f7dd08a757323181
atulchandradash <atulchandradash@gmail.com>
2023-09-23 17:58:03 +0800

    AddCost Section Update

--- app/Livewire/AddCost.php
+++ app/Livewire/AddCost.php
@@ -8,100 +8,81 @@ use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
+
 class AddCost extends Component
 {
-    public $user;
-    public $getTodayCost;
 
-    public $categoriesShow;
-    public $costRecord;
-
-    ////
-
-    public $btn = true;
-    //form
-    public $cost;
-    public $categories_id = '1';
-    public $date;
-
-    public $userid;
-
-
+    //all the input Fields
+    public $cost, $categories_id = '1', $date, $userid;
 
     public function addCost()
     {
+        $this->validate([
+            'userid' => 'required',
+            'categories_id' => 'required',
+            'date' => 'required',
+            'cost' => 'required|numeric|between:1,100000'
 
-        if ($this->cost != 0) {
-            $data = $this->validate([
-                'userid' => 'required',
-                'categories_id' => 'required',
-                'date' => 'required',
-                'cost' => 'required'
-            ]);
-            Cost::create($data);
+        ]);
 
-            $this->dispatch('successAlert', 'Cost added successfully!');
 
-            $this->cost = '';
-            $this->costRecord();
-        } else {
+        $data = new Cost();
+        $data->userid = $this->userid;
+        $data->categories_id = $this->categories_id;
+        $data->date = $this->date;
+        $data->cost = $this->cost;
 
-            session()->flash('message', 'Cost should not be 0 Or Blank');
-            session()->flash('messageType', 'danger');
-        }
-    }
+        $data->save();
 
-    public function costRecord()
-    {
-        //get Last Record
-        $this->costRecord = Cost::Where('userid', $this->user->id)->latest()->limit(5)->get();
-        //-----
+        $this->dispatch('successAlert', 'Cost added successfully!');
 
-        // getTime
-        date_default_timezone_set('Asia/Shanghai');
-        $timezone = date_default_timezone_get();
-        $now = Carbon::now('Asia/Shanghai');
-
-        // --------
-
-        // TdoayCostGet()
-        $this->getTodayCost = Cost::where('userid', $this->user->id)->where('date', $now->format('Y-m-d'))->sum('cost');
-        // ---------
-
+        $this->cost = '';
     }
 
     public function deleteCost($id)
     {
         if (isset($id)) {
-            $data = Cost::find($id);
-            $data->delete();
+            $deleteData = Cost::find($id);
+            $deleteData->delete();
 
             $this->dispatch('successAlert', "Cost Delete successfully!");
-
-            $this->costRecord();
         }
-    }
-
-    public function mount()
-    {
-        $this->user = Auth::user();
-
-        $this->userid = $this->user->id;
-
-
-        // categoriesShow()
-        $this->categoriesShow = Categorie::where('userid', 'ALL')->orWhere('userid', $this->user->id)->get();
-        // ------
-
-        $this->costRecord();
-
-        $this->date = date('Y-m-d');
     }
 
     public function render()
     {
 
-        return view('livewire.add-cost');
+        // getTime
+        date_default_timezone_set('Asia/Shanghai');
+        $timezone = date_default_timezone_get();
+        $now = Carbon::now('Asia/Shanghai');
+        // --------
+
+        $user = Auth::user();
+        //set the user id on the input fileds
+        $this->userid = $user->id;
+        //set the date in input fileds
+        $this->date = date('Y-m-d');
+        // show the categories in form
+        $categoriesShow = Categorie::where('userid', 'ALL')->orWhere('userid', $user->id)->get();
+        // ------
+
+        //get Last Record
+        $costRecord = Cost::Where('userid', $user->id)->latest()->limit(5)->get();
+        //-----
+
+
+
+        // TdoayCostGet()
+        $getTodayCost = Cost::where('userid', $user->id)->where('date', $now->format('Y-m-d'))->sum('cost');
+        // ---------
+
+        return view('livewire.add-cost', [
+            'costRecord' => $costRecord,
+            'getTodayCost' => $getTodayCost,
+            'user' => $user,
+            'categoriesShow' => $categoriesShow
+        ]);
     }
 
 
